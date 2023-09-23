667005bb3727fbf36465f523b383f9e095f70cc6
datocher <142504459+datocher@users.noreply.github.com>
2023-09-22 23:26:55 -0500

    P1.h

--- src/P1.h
+++ src/P1.h
@@ -4,6 +4,13 @@
 
 #ifndef PROG3_UNIT2_TEMPLATES_V2023_2_P1_H
 #define PROG3_UNIT2_TEMPLATES_V2023_2_P1_H
+#include <string>
 
-
+template <typename T = std::string>
+T input(const std::string& message = "") {
+    std::cout << message;
+    T value;
+    std::cin >> value;
+    return value;
+}
 #endif //PROG3_UNIT2_TEMPLATES_V2023_2_P1_H
