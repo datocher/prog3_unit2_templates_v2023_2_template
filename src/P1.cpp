4ee693b7bc4883e2ec91e93153a486a6f40a3615
datocher <142504459+datocher@users.noreply.github.com>
2023-09-22 23:27:53 -0500

    P1.cpp

--- src/P1.cpp
+++ src/P1.cpp
@@ -6,3 +6,14 @@
 #include "P1.h"
 using namespace std;
 
+int main() {
+    auto text = input();
+    auto entero = input<int>("Ingrese un numero: ");
+    auto real = input<double>("Ingrese un numero: ");
+    
+    std::cout << "El texto es: " << text << std::endl;
+    std::cout << "El entero es: " << entero << std::endl;
+    std::cout << "El real es: " << real << std::endl;
+
+    return 0;
+}
